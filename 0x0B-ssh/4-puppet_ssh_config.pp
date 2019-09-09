f2d28f86b2d625e66f84692fa6c94d7524bb41a1
alishabelle <alishabelle98@gmail.com>
2019-09-09 18:09:53 +0000

    fix output for checks

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # set up password config on sever with puppet
 file_line { 'password off':
-    ensure => present,
     path   => 'etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
-    match  => '^*.PasswordAuthentication yes$',
+    replace => true,
 }
 
 file_line { 'Holberton indentity key':
-    ensure => present,
     path   => 'etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton',
-    match  => '^IdentityFile',
+    replace => true,
 }
