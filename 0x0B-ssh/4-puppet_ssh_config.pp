6a0171d781d196a62db2433fb6a706ce10a9f27b
alishabelle <alishabelle98@gmail.com>
2019-09-09 18:13:33 +0000

    added backslash in path

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # set up password config on sever with puppet
 file_line { 'password off':
-    path   => 'etc/ssh/ssh_config',
+    path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
     replace => true,
 }
 
 file_line { 'Holberton indentity key':
-    path   => 'etc/ssh/ssh_config',
+    path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton',
     replace => true,
 }
