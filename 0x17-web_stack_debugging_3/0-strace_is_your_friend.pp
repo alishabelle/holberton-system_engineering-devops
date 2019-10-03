8ee8de50c235ab3573166b55b6d72abb0f7e118d
alishabelle <alishabelle98@gmail.com>
2019-10-03 04:12:21 +0000

    resubmitting task0 file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Manifest to automate error fix
 exec { 'replace phpp with php':
-  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-setting.php',
+  command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
