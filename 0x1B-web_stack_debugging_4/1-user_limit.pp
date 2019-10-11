7bee16ca685b0cc39e289c875869e7f58b8e195e
alishabelle <alishabelle98@gmail.com>
2019-10-11 18:53:12 +0000

    correct output

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
-# make requests higher
-exec {'limit_increase':
+# make requests limit higher on server
+exec {'limit_userrequests_increase':
      command => 'sed -i "s/5/6000/g; s/4/5000/g" /etc/security/limits.conf',
      path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
