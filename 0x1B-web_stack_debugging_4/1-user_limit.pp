d28ec664abf1b2b67ca9678e6a83828c7a906a63
alishabelle <alishabelle98@gmail.com>
2019-10-11 18:55:16 +0000

    fixed puppet issues

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # make requests limit higher on server
 exec {'limit_userrequests_increase':
-     command => 'sed -i "s/5/6000/g; s/4/5000/g" /etc/security/limits.conf',
-     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
+  command => 'sed -i "s/5/6000/g; s/4/5000/g" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
